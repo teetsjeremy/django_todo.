47c4ad5197348cdbef51d7b096f84d4597e0da55
Jeremy Teets <teets.jeremy@yahoo.com>
2020-01-06 09:01:44 +0000

    created form and button

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from todo.views import get_todo_list
+from todo.views import get_todo_list, create_an_item
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', get_todo_list)
+    url(r'^$', get_todo_list),
+    url(r'^add$', create_an_item)
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Item
 
 # Create your views here.
 
 def get_todo_list(request):
-    return render(request, "todo_list.html")    
+    results = Item.objects.all()
+    return render(request, "todo_list.html", {'items': results})
+
+def create_an_item(request):
+    if request.method=="POST":
+        new_item = Item()
+        new_item.name = request.POST.get('name')
+        new_item.done = 'done' in request.POST
+        new_item.save()
+
+        return redirect(get_todo_list)
+
+    return render(request, "item_form.html")
