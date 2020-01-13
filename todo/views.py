c46558585ec4463784e58566782e6b674fb1ed54
Ubuntu <ubuntu@ip-172-31-23-86.us-west-2.compute.internal>
2020-01-13 05:58:24 +0000

    updated heroku files

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello World")
+#def say_hello(request):
+#    return HttpResponse("Hello World")
+    
+    
+def get_todo_list(request):
+    return render(request, "todo_list.html")
